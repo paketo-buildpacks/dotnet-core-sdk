050ef161ecaf14cba832e6f60d4962f334a4191a
Forest Eckhardt <feckhardt@pivotal.io>
2020-07-15 10:54:43 -0400

    Fix integration tests

--- integration/integration_test.go
+++ integration/integration_test.go
@@ -273,8 +273,8 @@ dotnet-sdk:
 
 		Expect(app.StartWithCommand("dotnet dotnet.dll --urls http://0.0.0.0:${PORT}")).To(Succeed())
 
-		Expect(app.BuildLogs()).To(MatchRegexp(fmt.Sprintf(`dotnet-runtime.%s`, `2\.1\.18`)))
-		Expect(app.BuildLogs()).To(MatchRegexp(fmt.Sprintf(`dotnet-runtime.%s`, `2\.1\.18`)))
+		Expect(app.BuildLogs()).To(MatchRegexp(fmt.Sprintf(`dotnet-runtime.%s`, `2\.1\.19`)))
+		Expect(app.BuildLogs()).To(MatchRegexp(fmt.Sprintf(`dotnet-runtime.%s`, `2\.1\.19`)))
 
 		Eventually(func() string {
 			body, _, _ := app.HTTPGet("/")
@@ -297,8 +297,8 @@ dotnet-sdk:
 			app.SetHealthCheck("stat /workspace", "2s", "15s")
 		}
 
-		Expect(app.BuildLogs()).To(MatchRegexp(fmt.Sprintf(`dotnet-runtime_%s`, `2\.1\.19`)))
-		Expect(app.BuildLogs()).To(MatchRegexp(fmt.Sprintf(`dotnet-runtime_%s`, `2\.1\.19`)))
+		Expect(app.BuildLogs()).To(MatchRegexp(fmt.Sprintf(`dotnet-runtime_%s`, `2\.1\.20`)))
+		Expect(app.BuildLogs()).To(MatchRegexp(fmt.Sprintf(`dotnet-runtime_%s`, `2\.1\.20`)))
 
 		Expect(app.StartWithCommand("dotnet dotnet.dll --urls http://0.0.0.0:${PORT}")).To(Succeed())
 
